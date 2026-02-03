4903e78c44cd63fbff61f7aae00c4c8d24d5de78
IrinaRogacheva <omG901iime03!>
2026-02-04 02:43:05 +0300

    задания 4 модуля

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,12 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  const rows = table.rows;
+  for (let i = 0; i < rows.length; i++) {
+    const cells = table.rows[i].cells;
+    for (let j = 0; j < cells.length; j++) {
+      if (i === j) {
+        cells[j].style.backgroundColor = 'red';
+        break;
+      }
+    }
+  }
 }
