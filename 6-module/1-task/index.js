5e503a9d5587a5109aa0f435fa49d960880e343d
IrinaRogacheva <omG901iime03!>
2026-02-19 02:06:16 +0300

    6 модуль

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,41 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.render();
+  }
+
+  render() {
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = `<thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      ${this.rows.map(({ name, age, salary, city }) => `<tr>
+        <td>${name}</td>
+        <td>${age}</td>
+        <td>${salary}</td>
+        <td>${city}</td>
+        <td><button>X</button></td>
+      </tr>`)}
+    </tbody>`;
+
+    this.onClick();
+
+    return this.elem;
+  }
+
+  onClick() {
+    this.elem.addEventListener('click', (event) => {
+      if (event.target.closest('button')) {
+        event.target.closest('tr').remove();
+      }
+    });
   }
 }
